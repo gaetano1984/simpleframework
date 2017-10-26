7a1e5bc306c49d57a02ff10c6127405e8ee46490
gaetano campanella <gaetanocampanella@MBPdigaetano2.lan>
2017-10-26 23:43:03 +0200

    ignorato app.css

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,10 +1,14 @@
 gulp = require('gulp');
 sass = require('gulp-sass');
 concat = require('gulp-concat');
+ugly = require('gulp-uglifycss');
 
 gulp.task('compile-css', function(){
 	gulp.src('scss/*scss')
-		.pipe(sass({outputStyle: 'compressed'}))
+		//.pipe(sass({outputStyle: 'compressed'}))
 		.pipe(concat('app.css'))
+		.pipe(ugly({
+			"maxLineLen": 1,
+		}))
 		.pipe(gulp.dest('css'))
 });
