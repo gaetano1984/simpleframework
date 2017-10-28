ba1212d65d429eccfa85240377df11dcd34ba497
gaetano campanella <gaetanocampanella@MBPdigaetano2.lan>
2017-10-28 22:25:35 +0200

    cambiati pacchetti nodejs per compilazione dei file css

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,12 +1,10 @@
 gulp = require('gulp');
-sass = require('gulp-sass');
 concat = require('gulp-concat');
-ugly = require('gulp-uglifycss');
-mincss = require('gulp-minify-css');
+mincss = require('gulp-clean-css');
 
 gulp.task('compile-css', function(){
 	gulp.src('scss/*scss')
 		.pipe(concat('app.css'))
 		.pipe(mincss())
 		.pipe(gulp.dest('css'))
-});
+});	
