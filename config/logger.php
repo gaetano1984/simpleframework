054d7632250db69f981e35f3216577bfcc10cbe7
gaetano campanella <gaetanocampanella@MBPdigaetano2.lan>
2017-10-29 22:12:47 +0100

    corretta posizione dei file di log

--- config/logger.php
+++ config/logger.php
@@ -3,9 +3,9 @@
 	global $config_log;
 
 	$config_log = [
-		'general_log' => getenvval('LOGGER_GEN', './storage/logs/general_log_'.date('Ymd')),
-		'db_log' => getenvval('LOGGER_DB', './storage/logs/db_log_'.date('Ymd')),
-		'access_error_log' => getenvval('LOGGER_ACC_ERR', './storage/logs/access_error_log_'.date('Ymd'))
+		'general_log' => getenvval('LOGGER_GEN', '../storage/logs/general_log_'.date('Ymd')),
+		'db_log' => getenvval('LOGGER_DB', '../storage/logs/db_log_'.date('Ymd')),
+		'access_error_log' => getenvval('LOGGER_ACC_ERR', '../storage/logs/access_error_log_'.date('Ymd'))
 	];
 
 ?>
